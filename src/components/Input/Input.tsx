1afe2a4a41e5f8074290e7c52ea56901fabe0a33
Mustaphy <mustafamusaberkaydemirci@gmail.com>
2023-05-05 19:26:19 +0200

    Implemented different CSS styles for input based on props

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -2,9 +2,13 @@ import { InputProperties } from './input-types';
 import './Input.css'
 
 function Input(props: InputProperties) {
+  const getClassName = (): string => {
+    return `styleface-input styleface-${props.type}`;
+  }
+
   return (
     <>
-      <input id={props.id} className="styleface-input" type={props.type} value={props.value} onChange={props.onChange} />
+      <input id={props.id} className={getClassName()} type={props.type} value={props.value} onChange={props.onChange} />
     </>
   )
 }
